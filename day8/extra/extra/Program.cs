7688b76bf254e9deb05f1b1a9f9fabb81291d8e9
Sviridov Yaroslav <ys9617890962@gmail.com>
2019-12-09 00:16:42 +0300

    finish day 8

---
+++ day8/extra/extra/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace extra {
+    internal class Program {
+        public static void Main(string[] args) {
+            String s = File.ReadAllText("/home/spolutrean/adventofcode2019/day8/extra/extra/in.txt");
+            int w = 25, h = 6;
+            int[,] result = new int[h,w];
+            for (int i = 0; i < h; ++i) {
+                for (int j = 0; j < w; ++j) {
+                    result[i, j] = 2;
+                }
+            }
+            //9117554426
+            int perLayer = h * w;
+            int layersCount = s.Length / perLayer;
+            for (int l = 0; l < layersCount; ++l) {
+                for (int i = 0; i < h; ++i) {
+                    for (int j = 0; j < w; ++j) {
+                        int c = s[l * perLayer + i * w + j] - '0';
+                        if (result[i, j] == 2 && c != 2) {
+                            result[i, j] = c;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < h; ++i) {
+                for (int j = 0; j < w; ++j) {
+                    Console.Write(result[i,j] == 1 ? '█' : ' ');
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ day8/standard/standard/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace standard {
+    internal class Program {
+        public static void Main(string[] args) {
+            String s = File.ReadAllText("/home/spolutrean/adventofcode2019/day8/standard/standard/in.txt");
+            //9117554426
+            int ans = 0;
+            int mn = 26;
+            int perLayer = 6 * 25;
+            int layersCount = s.Length / perLayer;
+            for (int i = 0; i < layersCount; ++i) {
+                int[] cnt = new int[3] {0, 0, 0};
+                for (int j = 0; j < perLayer; ++j) {
+                    cnt[s[i * perLayer + j] - '0']++;
+                }
+
+                if (cnt[0] < mn) {
+                    mn = cnt[0];
+                    ans = cnt[1] * cnt[2];
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
